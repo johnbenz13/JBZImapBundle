35d20a46dadc3e37fb0607f4c42cae26a291fd92
Jonathan Bensaid <john@bensaidj.com>
2013-09-10 13:02:12 +0200

    Add config for imap service

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace JBZ\ImapBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('jbz_imap');
+
+        $rootNode
+            ->children()
+                ->scalarNode('host')->isRequired()->end()
+                ->scalarNode('username')->isRequired()->end()
+                ->scalarNode('password')->isRequired()->end()
+                ->scalarNode('ssl')->defaultValue(true)->end()
+                ->scalarNode('folder')->defaultValue('INBOX')->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
---
+++ DependencyInjection/JBZImapExtension.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace JBZ\ImapBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+class JBZImapExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.xml');
+
+        $container->setParameter('jbz_imap.host', $config['host']);
+        $container->setParameter('jbz_imap.username', $config['username']);
+        $container->setParameter('jbz_imap.password', $config['password']);
+        $container->setParameter('jbz_imap.ssl', $config['ssl']);
+        $container->setParameter('jbz_imap.folder', $config['folder']);
+    }
+}
